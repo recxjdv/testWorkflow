11969c07c945a15823b1aaa611cd0730071f54d4
recxjdv <james@recx.co.uk>
2026-04-30 09:53:21 +0100

    More docker escape

--- bin/recx.sh
+++ bin/recx.sh
@@ -3,6 +3,20 @@ set -euo pipefail
 
 apt-get update -qq && apt-get install -y -qq curl
 
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt install docker-ce-cli
 
 sleep 10000
 
